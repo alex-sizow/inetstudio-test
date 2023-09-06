4a42e6078ed4120873e011c0eeeede7b70fc2338
Alex <alex_sizow@outlook.com>
2023-09-06 14:20:50 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ContentBlock from './components/ContentBlock.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+	<div>
+		<v-main>
+			<ContentBlock />
+		</v-main>
+		<v-footer
+			app
+			v-bind="localAttrs">
+			<Footer />
+		</v-footer>
+	</div>
+</template>
+
+<style scoped></style>
